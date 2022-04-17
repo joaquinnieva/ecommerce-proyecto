97335b382b32d23e8c3f174cf95c118584e08775
joaquinnieva <joacoo.nieva@gmail.com>
2022-04-17 19:42:19 -0300

    fix footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,12 +15,12 @@ function Footer() {
         </div>
         <div className="contactame footer-links">
           <p>
-            <Link to="/ayuda" className="contact-ayuda">
+            <Link to="/help" className="contact-ayuda">
               {LINK_HELP}
             </Link>
           </p>
           <p>
-            <Link to="/ayuda" className="contact-ayuda">
+            <Link to="/help" className="contact-ayuda">
               {LINK_JOB}
             </Link>
           </p>
@@ -28,23 +28,23 @@ function Footer() {
         <div className="contactame footer-social">
           <p>{FOOTER_FOLLOWME}</p>
           <div className="img-cont-footer">
-            <Link to="/ayuda">
+            <Link to="/help">
               <img className="imgfooter" src={Images.twitter} alt="icon" />
             </Link>
-            <Link to="/ayuda">
+            <Link to="/help">
               <img className="imgfooter" src={Images.instagram} alt="icon" />
             </Link>
-            <Link to="/ayuda">
+            <Link to="/help">
               <img className="imgfooter" src={Images.facebook} alt="icon" />
             </Link>
-            <Link to="/ayuda">
+            <Link to="/help">
               <img className="imgfooter" src={Images.youtube} alt="icon" />
             </Link>
           </div>
         </div>
 
         <p className="copy-footer">
-          {FOOTER_DEVELOPED}
+          {FOOTER_DEVELOPED}&nbsp;
           <a href="https://joaquinnieva.github.io/joaquinnieva/" target="_blank" rel="noreferrer">
             {DEVELOPER_NAME}
           </a>
